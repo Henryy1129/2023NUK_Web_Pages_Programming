8b33af57f74b8522144e84d2fde7f76e8adefac2
Henry <renyi.hung@gmail.com>
2023-02-22 18:37:45 +0800

    upload week1 homework

---
+++ week1_A1081303_test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>0216_A1081303</title>
+    </head>
+
+    <body>
+        <?php
+         echo “Hello, World\n”;
+          ?>
+    </body>
+</html>
